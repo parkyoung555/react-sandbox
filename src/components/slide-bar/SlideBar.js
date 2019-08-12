a2f553119accca34597cd37fea2d7b546a22e1fc
Young Park <parkyoung555@gmail.com>
2019-08-12 11:58:09 -0500

    Improved inner border radius calculation, refactored slider component to use react states

--- src/components/slide-bar/SlideBar.js
+++ src/components/slide-bar/SlideBar.js
@@ -3,24 +3,28 @@ import './SlideBar.scss';
 
 export class SlideBar extends Component {
 
-    componentStates = {
+    stateClasses = {
         complete: '_complete',
         dragging: '_dragging'
     };
-    isMouseDown = false;
-    currentPercentage;
     initialFillPercent = 50;
+    state = {
+        isMouseDown: false,
+        currentPercentage: 0
+    };
 
     constructor(props) {
         super(props);
-
         this.initialFillPercent = this.props.initialFillPercent || this.initialFillPercent;
+        this.initialFillPercent = this.initialFillPercent < 100 ? this.initialFillPercent : 100;
+        this.state.currentPercentage = this.initialFillPercent;
         this._handleMouseDownEvent = this._handleMouseDownEvent.bind(this);
     }
 
     componentDidMount() {
         this._setSlideTextWidth();
         this._setCompleteStateClass();
+        this.refs.component.style.setProperty('--progress', this.state.currentPercentage / 100);
         window.addEventListener('resize', this._setSlideTextWidth.bind(this));
         window.addEventListener('mouseup', this._handleMouseUpEvent.bind(this));
         window.addEventListener('mousemove', this._handleMouseMoveEvent.bind(this));
@@ -31,6 +35,10 @@ export class SlideBar extends Component {
 
     componentWillUnmount() {
         window.removeEventListener('resize', this._setSlideTextWidth);
+        window.removeEventListener('mouseup', this._handleMouseUpEvent);
+        window.removeEventListener('mousemove', this._handleMouseMoveEvent);
+        window.removeEventListener('touchend', this._handleMouseUpEvent);
+        window.removeEventListener('touchmove', this._handleMouseMoveEvent);
     }
 
     _setSlideTextWidth() {
@@ -38,48 +46,57 @@ export class SlideBar extends Component {
     }
 
     _handleMouseDownEvent(event) {
-        this.isMouseDown = true;
-        this.refs.slideHandle.style.transition = 'none';
-        this.refs.component.classList.add(this.componentStates.dragging);
-
-        this._handleMouseMoveEvent(event);
+        event.persist();
+        this.setState({ isMouseDown: true }, () => {
+            this.refs.slideHandle.style.transition = 'none';
+            this.refs.component.classList.add(this.stateClasses.dragging);
+            this._handleMouseMoveEvent(event);
+        });
     }
 
     _handleMouseUpEvent() {
-        this.isMouseDown = false;
-        this.refs.slideHandle.style.transition = null;
-        this.refs.component.classList.remove(this.componentStates.dragging);
-
-        if (this.currentPercentage < 100) {
-            this.refs.slideHandle.style.width = `${this.initialFillPercent}%`;
+        if (this.state.isMouseDown) {
+            this.refs.slideHandle.style.transition = null;
+            this.refs.component.classList.remove(this.stateClasses.dragging);
+            if (this.state.currentPercentage < 100) {
+                this._setCurrentFillPercentage(this.initialFillPercent);
+            }
         }
+        this.setState({ isMouseDown: false });
     }
 
     _handleMouseMoveEvent(event) {
-        if (this.isMouseDown) {
+        if (this.state.isMouseDown) {
             const xPos = event.touches ? event.touches[0].clientX : event.clientX;
             const containerDims = this.refs.component.getBoundingClientRect();
-            this.currentPercentage = ((xPos - containerDims.left) / containerDims.width) * 100;
-            this.refs.slideHandle.style.width = `${this.currentPercentage > 100 ? 100 : (this.currentPercentage < 0 ? 0 : this.currentPercentage)}%`;
-
+            this._setCurrentFillPercentage(((xPos - containerDims.left) / containerDims.width) * 100);
             this._setCompleteStateClass();
         }
     }
 
     _setCompleteStateClass() {
-        if (this.currentPercentage >= 100) {
-            this.refs.component.classList.add(this.componentStates.complete);
+        if (this.state.currentPercentage >= 100) {
+            this.refs.component.classList.add(this.stateClasses.complete);
         } else {
-            this.refs.component.classList.remove(this.componentStates.complete);
+            this.refs.component.classList.remove(this.stateClasses.complete);
         }
     }
+
+    _setCurrentFillPercentage(fillPercentage) {
+        this.setState((state, props) => {
+            this.refs.component.style.setProperty('--progress', fillPercentage / 100);
+            return {
+                currentPercentage: fillPercentage > 100 ? 100 : (fillPercentage < 0 ? 0 : fillPercentage)
+            };
+        });
+    };
 
     render() {
         return (
             <div className={`slide-bar${this.props.className ? ` ${this.props.className}` : ''}`} ref="component">
                 <div className="slide-bar__wrapper" onMouseDown={this._handleMouseDownEvent} onTouchStart={this._handleMouseDownEvent}>
                     <div className="slide-bar__content">
-                        <div className="slide-bar__slide" style={{ width: `${this.initialFillPercent}%` }} ref="slideHandle">
+                        <div className="slide-bar__slide" style={{ width: `${this.state.currentPercentage}%` }} ref="slideHandle">
                             <span className="slide-bar__slide__text" ref="slideHandleText">{this.props.label}</span>
                         </div>
                         <div className="slide-bar__base" ref="slideBase">
